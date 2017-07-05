8243d320f3ead10f74f1839ca74f911c6e0c0809
Rob McCaffrey <rmccaffrey@mail.valenciacollege.edu>
2017-07-05 22:03:55 +0000

    tested writing output to the HTML document

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ var gotName = confirm("Your name is " + userName + "?");
 alert("Howdy, " + userName);
 
 console.log(gotName);
+
+//testing 2D arrays
+
+var roomNum = [150, 151, 152];
+var dayOfWeek = [roomNum, roomNum, roomNum, roomNum, roomNum];
+document.write (dayOfWeek[2][1]  + "<br>");
+
+//check what happens when I change one of the indecies in the top level array
+
+dayOfWeek[2][1] = "Building 9 Studio";
+document.write (dayOfWeek[2][1]  + "<br>");
+document.write (dayOfWeek[3][1]  + "<br>");
+
+//result: changing a value in the embedded array changes the value for each instance of the embedded array
