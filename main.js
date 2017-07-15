94079d996e4bdcc56f8f2f1e3a17ffd2ebfa1895
Rob McCaffrey <rmccaffrey@mail.valenciacollege.edu>
2017-07-15 09:39:45 +0000

    Wrote out ideas for object-based room scheduler

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+//initial test of the connection between main.js and index.html
 var userName = prompt("What's your name?");
 
 var gotName = confirm("Your name is " + userName + "?");
@@ -20,3 +21,11 @@ document.write (dayOfWeek[2][1]  + "<br>");
 document.write (dayOfWeek[3][1]  + "<br>");
 
 //result: changing a value in the embedded array changes the value for each instance of the embedded array
+
+/*test of using objects to build a room scheduler. Possible objects and 
+properties include ROOM{number, capacity, classAssigned}, TEACHER{prepped
+classes, load, availability}, CLASS{cap, duration, #preReqs, track, timeOfDay}.
+Possible code might include:
+if room.classAssigned is true, check class.duration to see next free time
+if class.timeOfDay is "evening", check teacher.availability
+if class.cop is > room.capacity, change class.cap to = room.capacity*/
