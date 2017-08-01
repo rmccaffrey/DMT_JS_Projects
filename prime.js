cac82ac6067e188e93c95354dd176a8a89601687
Rob McCaffrey <rmccaffrey@mail.valenciacollege.edu>
2017-08-01 15:06:07 +0000

    added comments about how to find prime numbers

--- prime.js
+++ prime.js
@@ -4,6 +4,19 @@ var number = 7;
 var dividedBy = number - 1;
 var listOfPrimes = [];
 
+
+/**
+ * this needs a number as input
+ * followed by a loop that divides the number by every number
+ * between it and the number 1. If any of those divisions has no 
+ * remainder, the number isn't prime and we should 'continue' to 
+ * the next number (input--). If every number smaller
+ * than the input leaves a remainder, the input number should be
+ * pushed to a 'prime numbers' array and we should continue to
+ * the next number (input--)
+*/
+
+/* second attempt at cycling through both number and its divider
 while (dividedBy > 1) {
 if (number % dividedBy != 0) {
     dividedBy--;
@@ -12,7 +25,9 @@ if (number % dividedBy != 0) {
     listOfPrimes.push(number);
  }
 }
-/*
+*/
+
+/* first attempt at cycling through both the numbers and their dividers
 for (var i = number; i > 1; i--) {
 for (var j = dividedBy; j > 1; j--) {
     if (number % dividedBy !== 0) {
